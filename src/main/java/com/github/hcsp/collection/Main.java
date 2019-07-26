96e86a3241155fc1acaf06b1168e5e65f392ff7b
catsme <845058547@qq.com>
2019-07-26 10:40:21 +0800

    full stack trace of errors

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,6 +1,11 @@
 package com.github.hcsp.collection;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Arrays;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
