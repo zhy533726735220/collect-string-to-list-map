43f6d570bc6be1c15f488a4687239643bf3d71b5
squall1744 <squall1744@163.com>
2019-07-28 11:16:21 +0800

    collect string to list map (#9)
    collect string to list map (#9)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,9 +1,12 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
+
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-
+import java.util.TreeMap;
+import java.util.Collections;
+import java.util.Arrays;
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
     // 返回一个从部门名到这个部门的所有用户的映射。同一个部门的用户按照年龄进行从小到大排序。
@@ -12,7 +15,23 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> map = new TreeMap<>();
+        for (User user: users) {
+            if (!map.containsKey(user.getDepartment())) {
+                List<User> list = new LinkedList<>();
+                list.add(user);
+                map.put(user.getDepartment(), list);
+            }else {
+                map.get(user.getDepartment()).add(user);
+            }
+        }
+
+        for (String key: map.keySet()) {
+            Collections.sort(map.get(key));
+        }
+        return map;
+    }
 
     public static void main(String[] args) {
         System.out.println(
