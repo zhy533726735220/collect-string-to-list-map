b9c30198e09f5008356728558233411d8a977d73
XiongAmao <onycat@live.cn>
2019-08-11 20:30:16 +0800

    Collect string to list map. (#19)
    Collect string to list map. (#19)

--- src/main/java/com/github/hcsp/collection/User.java
+++ src/main/java/com/github/hcsp/collection/User.java
@@ -2,7 +2,7 @@ package com.github.hcsp.collection;
 
 import java.util.Objects;
 
-public class User {
+public class User implements Comparable<User> {
     // 用户的id
     private final Integer id;
     // 用户的姓名
@@ -50,5 +50,10 @@ public class User {
     @Override
     public int hashCode() {
         return Objects.hash(id);
+    }
+
+    @Override
+    public int compareTo(User user) {
+        return Integer.compare(age, user.age);
     }
 }
