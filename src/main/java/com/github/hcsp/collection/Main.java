57a1f2b1ad33e89bd543d7cdea35d1fedc42d6c4
Mingru Li <limingru1991@gmail.com>
2019-08-16 16:40:06 +0800

    Collection练习 (#21)
    Collection练习 (#21)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,11 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +15,24 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        HashMap<String, List<User>> resultMap = new HashMap();
+
+        for (User u: users) {
+            String department = u.getDepartment();
+            if (resultMap.containsKey(department)) {
+                List<User> users1 = resultMap.get(department);
+                users1.add(u);
+                Collections.sort(users1);
+            } else {
+                ArrayList<User> list = new ArrayList();
+                list.add(u);
+                resultMap.put(department, list);
+            }
+        }
+
+        return resultMap;
+    }
 
     public static void main(String[] args) {
         System.out.println(
