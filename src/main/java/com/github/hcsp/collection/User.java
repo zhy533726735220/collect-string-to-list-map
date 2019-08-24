735fb0a988c2ac80058102b9131138e237b15d48
shiliyu <34638563+shiliyu@users.noreply.github.com>
2019-08-25 07:29:53 +0800

    Sort a given list and group by certain rule. (#26)
    Sort a given list and group by certain rule. (#26)

--- src/main/java/com/github/hcsp/collection/User.java
+++ src/main/java/com/github/hcsp/collection/User.java
@@ -51,4 +51,13 @@ public class User {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name=" + name +
+                ", department=" + department +
+                ", age=" + age +
+                '}';
+    }
 }
