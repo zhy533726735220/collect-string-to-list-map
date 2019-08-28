a3ec424e7da4cace138ec1f52cb6c84e2ec71b39
Rokixy <48944401+Rokixy@users.noreply.github.com>
2019-08-28 21:33:15 +0800

    List to Map (#29)
    List to Map (#29)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,12 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Comparator;
+import java.util.Arrays;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +16,30 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+
+        Map<String, List<User>> map = new HashMap<>();
+        List<User> usersList = new ArrayList<>(users);
+        usersList.sort(Comparator.comparingInt(User::getAge));
+
+        for (User user : usersList) {
+
+            String department = user.getDepartment();
+
+            if (!map.keySet().contains(user.getDepartment())) {
+
+                List<User> list = new ArrayList<>();
+                list.add(user);
+                map.put(user.getDepartment(), list);
+
+            } else {
+                map.get(department).add(user);
+            }
+
+        }
+
+        return map;
+    }
 
     public static void main(String[] args) {
         System.out.println(
