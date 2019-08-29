f4728ad6ca33285dc114b2a07bcb2091b9a58481
kungua <52815012+kungua@users.noreply.github.com>
2019-08-29 11:29:30 +0800

    collect string to list map (#30)
    collect string to list map (#30)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,6 +1,9 @@
 package com.github.hcsp.collection;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,7 +15,38 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> result = new HashMap<>();
+        // loop users
+        for (int i = 0; i < users.size(); i++) {
+            User user = users.get(i);
+            String department = user.getDepartment();
+            if (result.get(department) != null) {
+                List<User> userList = result.get(department);
+                userList.add(user);
+                userList.sort(new SortByAge());
+            } else {
+                List<User> userList = new ArrayList<>();
+                userList.add(user);
+                userList.sort(new SortByAge());
+                result.put(department, userList);
+            }
+        }
+        return result;
+    }
+
+    static class SortByAge implements Comparator<User> {
+
+        @Override
+        public int compare(User u1, User u2) {
+            if (u1.getAge() > u2.getAge()) {
+                return 1;
+            } else if (u1.getAge() < u2.getAge()) {
+                return -1;
+            }
+            return 0;
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println(
