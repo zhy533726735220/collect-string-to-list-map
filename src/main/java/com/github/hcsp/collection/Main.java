75d0e9158b38bdca2a1eb7876e0eb1d723ca4ff3
Richardhtw <htw233@gmail.com>
2019-09-09 21:16:27 +0800

    将List处理成Map<String, List> (#33)
    将List处理成Map<String, List> (#33)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,13 @@
 package com.github.hcsp.collection;
 
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +17,27 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        Map<String, List<User>> departmentMappingUser = new HashMap<>();
+        Set<String> departmentKeys = departmentMappingUser.keySet();
+        Comparator<User> c = Comparator.comparingInt(User::getAge);
+
+        // 按 department 存储 User
+        for (User user : users) {
+            if (!departmentKeys.contains(user.getDepartment())) {
+                departmentMappingUser.put(user.getDepartment(), new ArrayList<>());
+            }
+            departmentMappingUser.get(user.getDepartment()).add(user);
+        }
+
+        // 同一个部门的用户按照年龄升序排序
+        for (
+                String key : departmentKeys) {
+            departmentMappingUser.get(key).sort(c);
+        }
+
+        return departmentMappingUser;
+    }
 
     public static void main(String[] args) {
         System.out.println(
