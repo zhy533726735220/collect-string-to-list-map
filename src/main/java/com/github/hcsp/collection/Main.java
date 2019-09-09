c88171485ed4935ad6a5165c5ffed5188f10592d
Bo Zhang <bo@gradle.com>
2019-09-09 21:56:55 +0800

    Update Main.java

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
     // 返回一个从部门名到这个部门的所有用户的映射。同一个部门的用户按照年龄进行从小到大排序。
@@ -12,7 +13,9 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        return users.stream().sorted(java.util.Comparator.comparing(User::getAge)).collect(Collectors.groupingBy(User::getDepartment));
+    }
 
     public static void main(String[] args) {
         System.out.println(
