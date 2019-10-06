329f45058efd331f09ff32a207119ba5027ba256
orangec <604150318@qq.com>
2019-10-06 18:03:16 +0800

    Collection: 将List处理成Map<String, List>

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -18,21 +18,21 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {
+    public static Map<String,List<User>> collect(List<User> users){
         Map<String,List<User>> map = new HashMap<>();
-        Set<String> department =new HashSet<>();
+        Set<String> department = new HashSet<>();
 
         for(User user: users){
             department.add(user.getDepartment());//先拿到一个部门的set集合类
         }
         for(String value:department){
             List<User> list = new ArrayList<>();
-            for(User user: users){//双重循环语句遍历部门和人,判断这个人是否是这个部门的
+            for(User user:users){//双重循环语句遍历部门和人,判断这个人是否是这个部门的
                 if(value.equals(user.getDepartment())){
                     list.add(user);
-                    list.sort(new Comparator<User>() {
+                    list.sort(new Comparator<User>(){
                         @Override
-                        public int compare(User user,User t1) {
+                        public int compare(User user,User t1){
                             if(user.getAge()>t1.getAge()){
                                 return 1;
                             }else if(user.getAge()==0||t1.getAge()==0||user.getAge()==t1.getAge()){
@@ -50,13 +50,12 @@ public class Main {
         return map;
     }
 
-    public static void main(String  [] args) {
+    public static void main(String [] args){
         System.out.println(
                 collect(
                         Arrays.asList(
                                 new User(1, "张三", 40, "技术部"),
                                 new User(2, "李四", 30, "技术部"),
-                                new User(3, "王五", 40, "市场部"),
-                                new User(4, "王二", 20, "技术部"))));
+                                new User(3, "王五", 40, "市场部"))));
     }
 }
