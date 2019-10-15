9f654a1522988997e674502d0ed6d911a483902c
Yuanmeng Xia <xymreasonxxs@163.com>
2019-10-15 19:06:43 +0800

    Collection to Map (#49)
    Collection to Map (#49)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,13 @@
 package com.github.hcsp.collection;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +17,19 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        List<User> orderedUsers = new ArrayList<>(users);
+        orderedUsers.sort(Comparator.comparingInt(User::getAge));
+        Map<String, List<User>> res = new HashMap<>();
+        Set departmentSet = new HashSet();
+        for (User user : orderedUsers) {
+            if (departmentSet.add(user.getDepartment())) {
+                res.put(user.getDepartment(), new ArrayList<>());
+            }
+            res.get(user.getDepartment()).add(user);
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
         System.out.println(
