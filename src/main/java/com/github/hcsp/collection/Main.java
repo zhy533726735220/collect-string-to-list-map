e71c241f52fd028530a9f873abf7b6a22cdecc63
Yingjie Li <liyingjie@protonmail.com>
2019-10-18 22:07:48 +0800

    feat: convert list to map

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,10 @@
 package com.github.hcsp.collection;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +14,9 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        return users.stream().sorted(Comparator.comparing(User::getAge)).collect(Collectors.groupingBy(User::getDepartment));
+    }
 
     public static void main(String[] args) {
         System.out.println(
