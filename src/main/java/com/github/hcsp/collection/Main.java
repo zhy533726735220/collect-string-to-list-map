9ab7833f2cb30ae76811d68fa1e43ce6ec5e7deb
stanley <154892616@qq.com>
2019-10-19 22:55:55 +0800

    List<User> transform to Map<String,List<User>>

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,7 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +11,14 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        List<User> sortedUsers = new ArrayList<>(users);
+        sortedUsers.sort(Comparator.comparingInt(User::getAge));
+        Map<String, List<User>> usersGroupByDepartment = sortedUsers.stream().collect(Collectors.groupingBy(User::getDepartment));
+        return usersGroupByDepartment;
+    }
+
+
 
     public static void main(String[] args) {
         System.out.println(
