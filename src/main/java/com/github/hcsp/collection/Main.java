71146217fb8fb34fb4dd184c5ca4280b2881947a
wangyujie1207 <82414199@163.com>
2019-10-29 11:22:06 +0800

    =号格式 (#55)
    =号格式 (#55)

--- src/main/java/com/github/hcsp/collection/Main.java
+++ src/main/java/com/github/hcsp/collection/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.collection;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     // 请编写一个方法，对传入的List<User>进行如下处理：
@@ -12,7 +10,22 @@ public class Main {
     // 返回如下映射：
     //    技术部 -> [{name=李四, department=技术部, age=30 }, {name=张三, department=技术部, age=40 }]
     //    市场部 -> [{name=王五, department=市场部, age=40 }]
-    public static Map<String, List<User>> collect(List<User> users) {}
+    public static Map<String, List<User>> collect(List<User> users) {
+        HashMap<String, List<User>> resultMap = new HashMap();
+        for (User user: users) {
+            String userDepartment = user.getDepartment();
+            if (resultMap.containsKey(userDepartment)) {
+                List<User> userDepartmentList = resultMap.get(userDepartment);
+                userDepartmentList.add(user);
+                Collections.sort(userDepartmentList);
+            } else {
+                ArrayList<User> list = new ArrayList();
+                list.add(user);
+                resultMap.put(userDepartment, list);
+            }
+        }
+        return resultMap;
+    }
 
     public static void main(String[] args) {
         System.out.println(
--- src/main/java/com/github/hcsp/collection/User.java
+++ src/main/java/com/github/hcsp/collection/User.java
@@ -2,7 +2,7 @@ package com.github.hcsp.collection;
 
 import java.util.Objects;
 
-public class User {
+public class User implements Comparable<User> {
     // 用户的id
     private final Integer id;
     // 用户的姓名
@@ -33,6 +33,22 @@ public class User {
 
     public String getDepartment() {
         return department;
+    }
+
+    @Override
+    public String toString() {
+        return "{name=" + name + ", department=" + department + ", age=" + age + "}";
+    }
+
+    @Override
+    public int compareTo(User user) {
+        if (age > user.age) {
+            return 1;
+        } else if (age < user.age) {
+            return -1;
+        } else {
+            return id.compareTo(user.id);
+        }
     }
 
     @Override
